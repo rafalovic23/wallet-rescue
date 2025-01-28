30e30ca14e75218c4de3e655fc1b70f86a71646f
Maxence <rafalovic.pro@gmail.com>
2025-01-28 22:20:40 +0100

    Initial commit: Jour 1 setup avec Next.js + Privy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import { PrivyProvider } from '@privy-io/react-auth'
+import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <PrivyProvider
+      appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID!}
+      config={{
+        loginMethods: ['twitter', 'google'],
+        appearance: {
+          theme: 'light',
+          accentColor: '#4A90E2'
+        }
+      }}
+    >
+      <Component {...pageProps} />
+    </PrivyProvider>
+  )
 }
+
+export default MyApp
